3fbddee31c8e3d2ad56dc292601464e34b65aa38
Berkay Ergin <berkayergin99@gmail.com>
2024-08-02 01:41:16 +0300

    Extract Header into its own component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Header from "@components/Header";
+
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <header>Hancocktail</header>
+      <Header />
+      <section></section>
     </main>
   );
 }
---
+++ components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <hgroup>
+        <p>There are cocktails.</p>
+        <p>There are super cocktails.</p>
+        <p>And then there are...</p>
+        <h1>Hancocktails</h1>
+      </hgroup>
+    </header>
+  );
+}
